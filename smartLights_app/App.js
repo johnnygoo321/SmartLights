179d478a1a9c87caeece4e22d4192a971424b440
Johnny G <johnnygoo321@gmail.com>
2023-11-28 19:49:23 -0500

    Updated Strip Configuration

--- smartLights_app/App.js
+++ smartLights_app/App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native";
 import SmartLights from "./components/pages/SmartLights";
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#151621", zIndex: 0 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#151621" }}>
       <SmartLights />
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
--- smartLights_app/components/pages/SmartLights.js
+++ smartLights_app/components/pages/SmartLights.js
@@ -11,13 +11,22 @@ import RoundedButton from "../buttons/RoundedButton";
 const { width, height } = Dimensions.get("window");
 
 const SmartLights = () => {
-  const [state, setState] = useState({ red: 255, green: 118, blue: 0 });
+  const [rgbValue, setRgbValue] = useState({ red: 255, green: 118, blue: 0 });
+  const [loading, setLoadingState] = useState(false);
 
-  const configureLedStrip = (endpoint) => {
+  const configureLedStrip = async (endpoint) => {
     let baseUrl = IPV4_ADDRESS_OF_PI + ":" + SERVER_PORT + "/" + endpoint,
-      data = { red: state.red, green: state.green, blue: state.blue };
+      data = { red: rgbValue.red, green: rgbValue.green, blue: rgbValue.blue };
 
-    axios
+    setLoadingState(true);
+    Toast.show({
+      type: "info",
+      text1: "Processing Request",
+      text2: "Please wait...",
+      autoHide: false,
+    });
+
+    await axios
       .post(baseUrl, data, {
         headers: {
           "Content-Type": "application/json",
@@ -26,27 +35,62 @@ const SmartLights = () => {
       })
       .then(
         (response) => {
-          // Toast.show({
-          //   type: "success",
-          //   text1: "TEST",
-          //   visibilityTime: 2000,
-          // });
+          console.log(response);
+          setLoadingState(false);
+          Toast.show({
+            type: "success",
+            text1: response.data,
+            text2: "Animation Selected: " + endpoint,
+            visibilityTime: 2500,
+          });
         },
         (error) => {
-          // Toast.show({
-          //   type: "error",
-          //   text1: "Could not configure light settings.",
-          //   text2:
-          //     "Ensure your Pi is on and connected to the Smart Light Device.",
-          //   visibilityTime: 2500,
-          // });
-          console.log(error);
+          console.log("Miserable Failure Error: ", error);
+          setLoadingState(false);
+          Toast.show({
+            type: "error",
+            text1: "Could not configure LED Strip.",
+            text2: "Check Error logs...",
+            visibilityTime: 2500,
+          });
         }
       );
   };
 
   const toastConfig = {
     success: ({ text1, text2, ...rest }) => (
+      <BaseToast
+        {...rest}
+        style={{ borderLeftColor: "lightgreen", backgroundColor: "#fff" }}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        text1Style={{
+          fontSize: width / 22,
+        }}
+        text2Style={{
+          color: "#000",
+          fontSize: width / 25,
+        }}
+        text1={text1}
+        text2={text2}
+      />
+    ),
+    error: ({ text1, text2, ...rest }) => (
+      <BaseToast
+        {...rest}
+        style={{ borderLeftColor: "red", backgroundColor: "#fff" }}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        text1Style={{
+          fontSize: width / 22,
+        }}
+        text2Style={{
+          color: "#000",
+          fontSize: width / 25,
+        }}
+        text1={text1}
+        text2={text2}
+      />
+    ),
+    info: ({ text1, text2, ...rest }) => (
       <BaseToast
         {...rest}
         style={{ borderLeftColor: "#FF9900", backgroundColor: "#fff" }}
@@ -62,68 +106,56 @@ const SmartLights = () => {
         text2={text2}
       />
     ),
-
-    error: ({ text1, text2, ...rest }) => (
-      <BaseToast
-        {...rest}
-        style={{ borderLeftColor: "#FF9900", backgroundColor: "#fff" }}
-        contentContainerStyle={{ paddingHorizontal: 15 }}
-        text1Style={{
-          fontSize: width / 21,
-        }}
-        text2Style={{
-          color: "#000",
-          fontSize: width / 26,
-        }}
-        text1={text1}
-        text2={text2}
-      />
-    ),
   };
 
   return (
-    <View
-      style={{
-        alignItems: "center",
-      }}
-    >
-      <Toast style={{ zIndex: 1 }} config={toastConfig} />
-      <ColorPicker
-        sliderComponent={Slider}
-        hideSliders={true}
-        defaultColor={"orange"}
-        onColorSelected={() => configureLedStrip("noEffect")}
-        onColorChange={(color) => setState(hexRgb(fromHsv(color)))}
-        style={{ width: width, height: height / 2.3 }}
-      />
-      <RoundedButton
-        onPress={() => configureLedStrip("wipe")}
-        buttonText="Wipe"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("rainbow")}
-        buttonText="Rainbow"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("rainbowCycle")}
-        buttonText="Rainbow Cycle"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("theaterChase")}
-        buttonText="Theater Chase"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("theaterChaseRainbow")}
-        buttonText="Theater Chase Rainbow"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("randomize")}
-        buttonText="Randomize"
-      ></RoundedButton>
-      <RoundedButton
-        onPress={() => configureLedStrip("clear")}
-        buttonText="Off"
-      ></RoundedButton>
+    <View pointerEvents={loading}>
+      <View style={{ zIndex: 1 }}>
+        <Toast config={toastConfig} />
+      </View>
+      <View
+        style={{
+          zIndex: 0,
+          alignItems: "center",
+        }}
+      >
+        <ColorPicker
+          sliderComponent={Slider}
+          hideSliders={true}
+          defaultColor={"orange"}
+          onColorSelected={() => configureLedStrip("noEffect")}
+          onColorChange={(color) => setRgbValue(hexRgb(fromHsv(color)))}
+          style={{ width: width, height: height / 2.3 }}
+        />
+        <RoundedButton
+          onPress={() => configureLedStrip("wipe")}
+          buttonText="Wipe"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("rainbow")}
+          buttonText="Rainbow"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("rainbowCycle")}
+          buttonText="Rainbow Cycle"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("theaterChase")}
+          buttonText="Theater Chase"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("theaterChaseRainbow")}
+          buttonText="Theater Chase Rainbow"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("randomize")}
+          buttonText="Randomize"
+        ></RoundedButton>
+        <RoundedButton
+          onPress={() => configureLedStrip("clear")}
+          buttonText="Off"
+        ></RoundedButton>
+      </View>
     </View>
   );
 };
